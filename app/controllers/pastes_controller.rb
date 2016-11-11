aa315daead27b93f6d968be426182dd75d1fcaf5
Alan Kiš <alan.kish38@gmail.com>
2016-11-11 12:34:25 +0100

    Add basic CRUD to Paste model

--- app/controllers/pastes_controller.rb
+++ app/controllers/pastes_controller.rb
@@ -1,4 +1,20 @@
 class PastesController < ApplicationController
+  def show
+    @paste = Paste.find(params[:id])
+  end
+  
   def new
   end
+
+  def create
+    @paste = Paste.new(paste_params)
+
+    @paste.save
+    redirect_to @paste
+  end
+
+  private
+    def paste_params
+      params.require(:paste).permit(:paste_content)
+    end
 end
